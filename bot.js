081d963bab03aba481b0b935e87765b823b91c91
erobororo <43712406+erobororo@users.noreply.github.com>
2018-09-30 03:10:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on("message", (message) => {
 
 
   if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("Hello, I’m Sasaki Haise, your new assistant! The best way to reach me is by typing s! and another word, but feel free to keep chatting like normal. I look forward to working with you!");
+    message.channel.send("Hello! I’m Sasaki Haise, your new assistant. The best way to reach me is by typing s! and another word, but feel free to keep chatting like normal. I look forward to working with you!");
   } 
 
 });
@@ -29,10 +29,14 @@ client.on("message", (message) => {
   if (message.author.bot) return;
 
 
-  if (message.content.includes.toLowerCase().includes("I love you, Haise")) { 
+  if (message.content.toLowerCase().includes("I love you, Haise")) { 
     const replies = [".........Oh.", "W-what!?", "Eh?", "U-um...!", "Oh....um, thank you...", "That’s very kind of you...", "Ah....", "P-please, not at work...!", "Um?!", "Eh???", "M-me?", "I.......love you too.", "...let’s talk about that later.", "Are you sure...?", "Ha............", "Mh..??", "......Come again?"] message.replytext = Math.floor((Math.random() * replies.length) + 0); message.channel.send(replies[message.replytext]); 
 
 }
+
+
+
+
 
    
 });
