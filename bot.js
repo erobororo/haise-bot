97c69c2a15977c97260c19d18a37b7b8a706ce43
erobororo <43712406+erobororo@users.noreply.github.com>
2018-09-30 04:14:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,19 +7,7 @@ client.on("ready", () => {
 });
 
 
-let prefix = "s!";
 
-
-client.on("message", (message) => {
-
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-
-  if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("Hello! I’m Sasaki Haise, your new assistant. The best way to reach me is by typing s! and another word, but feel free to keep chatting like normal. I look forward to working with you!");
-  } 
-
-});
 
 
 function randomMessage(){
@@ -31,19 +19,52 @@ function randomMessage(){
         case 3: return "U-um...!";
         case 4: return "Oh....um, thank you...";
         case 5: return "That's very kind of you...";
-        case 6: return "Ah..."
-        case 7: return "M-me?"
-        case 8: return "...Let's talk about that later."
-        case 9: return "Are you sure...?"
-        case 10: return "...Come again?"
-        case 11: return "Haa........."
-        case 12: return "I......ah..."
+        case 6: return "Ah...";
+        case 7: return "M-me?";
+        case 8: return "...Let's talk about that later.";
+        case 9: return "Are you sure...?";
+        case 10: return "...Come again?";
+        case 11: return "Haa.........";
+        case 12: return "I......ah...";
     }
 }
+
+
+function randomImage(){
+    var randomNumber = Math.round(Math.random()*4);
+    switch(randomNumber){
+        case 0: return "https://cdn.discordapp.com/attachments/495806477995343882/495877482155606035/Screen_Shot_2018-09-28_at_7.41.57_PM.png";
+        case 1: return "https://media.giphy.com/media/CZpro4AZHs436/giphy.gif";
+        case 2: return "https://media.giphy.com/media/CZpro4AZHs436/giphy2.gif";
+        case 3: return "https://media.giphy.com/media/CZpro4AZHs436/giphy3.gif";
+    }
+}
+
+let prefix = "s!";
+
+
+client.on("message", (message) => {
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+
+  if (message.content.startsWith(prefix + "help")) {
+    message.channel.send("Hello, I’m Sasaki Haise, your new assistant. The best way to reach me is by typing s! and another word, but feel free to keep chatting like normal. I look forward to working with you!");
+  } else
+
+
+  if (message.content.startsWith(prefix + "selfie")) {
+    message.channel.send(randomImage());
+  }
+
+});
+
+
 
 client.on("message", (message)=>{ 
 
   if (message.author.bot) return;
+
 
 
   if (message.content.includes("i love you haise")) {
@@ -103,17 +124,10 @@ client.on("message", (message)=>{
 
   if (message.content.includes("I love you, Sas")) {
     message.channel.send(randomMessage());
-  } else
-
-
-
-
-
-
-
-  if (message.content.includes("what do you look like")) {
-    message.channel.send("https://cdn.discordapp.com/attachments/495806477995343882/495877482155606035/Screen_Shot_2018-09-28_at_7.41.57_PM.png");
   } 
+
+
+
 
 });
 
