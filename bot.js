cc3f44958cb2cfbfeba0386fd82f36bc323cf561
erobororo <43712406+erobororo@users.noreply.github.com>
2018-09-30 03:21:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+function randomMessage(){
+    var randomNumber = Math.round(Math.random()*2); // 0, 1 or 2
+    switch(randomNumber){
+        case 0: return 'Hello!';
+        case 1: return 'Bye!';
+        case 2: return 'Random';
+    }
+}
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
@@ -21,25 +30,32 @@ client.on("message", (message) => {
 });
 
 
+robot.on('message', (message)=>{ 
+
+    if (message.content == "%ex") {
+    message.channel.send(randomMessage());
+    }
+});
 
 
+// client.on("message", (message) => {
 
-client.on("message", (message) => {
-
-  if (message.author.bot) return;
+//  if (message.author.bot) return;
 
 
-  if (message.content.toLowerCase().includes("I love you, Haise")) { 
-    const replies = [".........Oh.", "W-what!?", "Eh?", "U-um...!", "Oh....um, thank you...", "That’s very kind of you...", "Ah....", "P-please, not at work...!", "Um?!", "Eh???", "M-me?", "I.......love you too.", "...let’s talk about that later.", "Are you sure...?", "Ha............", "Mh..??", "......Come again?"] message.replytext = Math.floor((Math.random() * replies.length) + 0); message.channel.send(replies[message.replytext]); 
+ // if (message.content.toLowerCase().includes("I love you, Haise")) { 
+   // const replies = [".........Oh.", "W-what!?", "Eh?", "U-um...!", "Oh....um, thank you...", "That’s very kind of you...", "Ah....", "P-please, not at work...!", "Um?!", "Eh???", "M-me?", "I.......love you too.", "...let’s talk about that later.", "Are you sure...?", "Ha............", "Mh..??", "......Come again?"] 
+    // message.replytext = Math.floor((Math.random() * replies.length) + 0); 
+    // message.channel.send(replies[message.replytext]); 
 
-}
+// }
 
 
 
 
 
    
-});
+// });
 
 
 
